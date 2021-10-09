65b6574aed222a77c00558a1269d1611d42de620
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-10-09 22:32:21 +0300

    подключение клиентских скриптов

---
+++ 3/public/app.js
@@ -0,0 +1,9 @@
+// клиентский скрпит
+
+// для каждого 
+document.querySelectorAll('.price').forEach( node =>{
+    node.textContent = new Intl.NumberFormat("ru-RU", {
+        currency: "rub",
+        style: "currency"
+    }).format(node.textContent);
+});
