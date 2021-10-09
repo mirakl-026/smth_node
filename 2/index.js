fa36b9947e7321bdbf2cc8b340ca7ff2d26465eb
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-10-09 11:28:24 +0300

    вынос во внешний html + fs + path

--- 2/index.js
+++ 2/index.js
@@ -1,5 +1,7 @@
 // Модуль http
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 
 
@@ -7,38 +9,50 @@ const server = http.createServer((req, res) => {
     if(req.method === "GET") {
         // указываем content type
         res.writeHead(200, {
-            "Content-Type":"text/html"  // тип контента - то как браузер воспринимает ответ
+            "Content-Type":"text/html; charset=utf-8"  // тип контента - то как браузер воспринимает ответ
         });
 
-        res.end(`
-            <h1>Form</h1>
-            <form method="post" action="/">
-                <input name="title" type="text" />
-                <button type="submit">Send</button>
-            </form>
-        `);
+        if (req.url === "/") {
+            // index.html
+            fs.readFile(
+                path.join(__dirname, "views", "index.html"),
+                "utf-8",
+                (err, content) => {
+                    if (err) throw new Error(err);
+
+                    res.end(content);
+                }
+            );
+        } else if (req.url === "/about") {
+            // about.html
+            fs.readFile(
+                path.join(__dirname, "views", "about.html"),
+                "utf-8",
+                (err, content) => {
+                    if (err) throw new Error(err);
+
+                    res.end(content);
+                }
+            );
+        }
+
     } else if (req.method === "POST") {
-        // echo
-        // получим сообщение пользователя
+
         const body = [];    // тело запроса - массив 
         res.writeHead(200, {
             "Content-Type": "text/html; charset=utf-8" // тип ответа и кодировка
         });
-        // chunk - порция данных/параметров
-        // req - наследник EventEmitter, у него есть событие data - Buffer, тот самый
+
         req.on("data", (data) => {
             body.push(Buffer.from(data));   // записываем данные в массив
         });
 
-        // по завершению передачи все данные будут в массиве body
         req.on("end", () => {
-            //console.log(body.toString()); // title=Jeka
             const message = body.toString().split("=")[1];
 
             res.end(`
                 <h1>Ваше сообщение: ${message} </h1>
             `);
-
         });
     }
 });
