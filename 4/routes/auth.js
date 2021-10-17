05574ec980e5855f52832405636ab06ec797970c
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-10-17 14:28:39 +0300

    замена пороля

--- 4/routes/auth.js
+++ 4/routes/auth.js
@@ -203,4 +203,32 @@ router.get("/password/:token",  async (req, res) =>{
 
 });
 
+
+router.post("/password", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            _id: req.body.userId,
+            resetToken: req.body.token,
+            resetTokenExp: {$gt: Date.now()}
+        })
+
+        if (user) {
+            user.password = await bcrypt.hash(req.body.password, 10);
+            user.resetToken = undefined;
+            user.resetTokenExp = undefined;
+            await user.save();
+
+            res.redirect("/auth/login");
+
+        } else {
+            req.flash("loginError", "Время жизни токена истекло...");
+            res.redirect("/auth/login");
+        }
+
+
+    } catch (e) {
+        console.log(e);
+    }
+})
+
 module.exports = router;
