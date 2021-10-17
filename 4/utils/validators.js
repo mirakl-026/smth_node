2298b3a3220c8c5f4e547f0a366f924e1a40bb05
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-10-17 17:20:53 +0300

    санитайзеры

--- 4/utils/validators.js
+++ 4/utils/validators.js
@@ -2,22 +2,35 @@ const {body} = require("express-validator");
 const User = require("../models/user");
 
 exports.registerValidators = [
-    body("email").isEmail().withMessage("Введите корректный email").custom( async (value, {req}) => {
-        try {
-            const user = await User.findOne({ email: value });
-            if (user) {
-                return Promise.reject("Пользователь уже существует");
+    body("email")
+        .isEmail()
+        .withMessage("Введите корректный email")
+        .custom( async (value, {req}) => {
+            try {
+                const user = await User.findOne({ email: value });
+                if (user) {
+                    return Promise.reject("Пользователь уже существует");
+                }
+            } catch(e) {
+                console.log(e);
             }
-        } catch(e) {
-            console.log(e);
-        }
-    }),
-    body("password", "Пароль: лат.буквы и цифры").isLength({min:2, max:56}).isAlphanumeric(),
-    body("confirm").custom((value, {req}) => {
-        if (value !== req.body.password) {
-            throw new Error( "Пароли должны совпадать");
-        }
-        return true;
-    }),
-    body("name").isLength({min: 3}).withMessage("Имя минимум 3 символа")
+        })
+        .normalizeEmail(),  // санитайзер - приводит к разным регистрам в соответсвии с держателями почты
+    body("password", "Пароль: лат.буквы и цифры")
+        .isLength({min:2, max:56})
+        .isAlphanumeric()
+        .trim(),    // санитайзер - удаляет пробелы по краям
+    body("confirm")
+        .custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error( "Пароли должны совпадать");
+            }
+            return true;
+        })
+        .trim(),
+    body("name")
+        .isLength({min: 3})
+        .withMessage("Имя минимум 3 символа")
+        .trim()
+
 ]
